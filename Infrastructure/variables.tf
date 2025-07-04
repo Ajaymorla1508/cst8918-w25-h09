2faad4afd59d438cf59da6de7d6adc95b32a5271
AjayMorla02 <morlaajay421@gmail.com>
2025-07-03 20:08:15 -0400

    first commit

---
+++ Infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-cst8918-aks"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "aks_cluster_name" {
+  default = "aks-cst8918-cluster"
+}
